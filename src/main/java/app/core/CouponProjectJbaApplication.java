a6895febe55fd1a658907b69cfcc8e030670636f
'Asher <'asherplotnik@gmail.com'>
2021-04-26 12:43:45 +0700

    failed

--- src/main/java/app/core/CouponProjectJbaApplication.java
+++ src/main/java/app/core/CouponProjectJbaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import app.core.couponProjectExceptions.DaoException;
-import app.core.filters.SpaFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -16,14 +15,4 @@ public class CouponProjectJbaApplication {
 	public static void main(String[] args) throws DaoException {
 		SpringApplication.run(CouponProjectJbaApplication.class, args);
 	}
-	
-	@Bean
-	public FilterRegistrationBean<SpaFilter> SpaFilterRegistration(){
-		FilterRegistrationBean<SpaFilter> filterRegistrationBean = new FilterRegistrationBean<SpaFilter>();
-		SpaFilter spaFilter = new SpaFilter();
-		filterRegistrationBean.setFilter(spaFilter);
-		filterRegistrationBean.addUrlPatterns("/*");
-		return filterRegistrationBean;
-	}
-	
 }
