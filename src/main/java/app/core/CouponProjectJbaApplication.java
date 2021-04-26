8111c20750f9105c41a82be210cf653bfc005fde
'Asher <'asherplotnik@gmail.com'>
2021-04-26 12:11:40 +0700

    spa test 3

--- src/main/java/app/core/CouponProjectJbaApplication.java
+++ src/main/java/app/core/CouponProjectJbaApplication.java
@@ -2,8 +2,11 @@ package app.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import app.core.couponProjectExceptions.DaoException;
+import app.core.filters.SpaFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,6 +15,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class CouponProjectJbaApplication {
 	public static void main(String[] args) throws DaoException {
 		SpringApplication.run(CouponProjectJbaApplication.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean<SpaFilter> SpaFilterRegistration(){
+		FilterRegistrationBean<SpaFilter> filterRegistrationBean = new FilterRegistrationBean<SpaFilter>();
+		SpaFilter spaFilter = new SpaFilter();
+		filterRegistrationBean.setFilter(spaFilter);
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
 	}
 	
 }
---
+++ src/main/java/app/core/filters/SpaFilter.java
@@ -0,0 +1,39 @@
+package app.core.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SpaFilter implements Filter{
+	
+
+	public SpaFilter() {
+		super();
+	}
+
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;	
+		String path = req.getRequestURI();
+	    if ("/health".equals(path)) {
+	    	chain.doFilter(request, response);
+	    	return;
+		} else {
+	    	res.sendRedirect("/");
+	    	chain.doFilter(request, res);
+	    }
+	
+	}
+	
+
+}
+
