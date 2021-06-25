082b2e8fe41b81c18332bd7402ba8ca796795ebe
'Asher <'asherplotnik@gmail.com'>
2021-06-25 22:25:07 +0700

    yo

--- src/main/java/app/core/filters/TokenFilter.java
+++ src/main/java/app/core/filters/TokenFilter.java
@@ -22,6 +22,7 @@ public class TokenFilter implements Filter{
 	}
 	
 	public void setHeaders(HttpServletResponse res) {
+		res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 		res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Headers","*");
         res.setHeader("Access-Control-Expose-Headers","*");
